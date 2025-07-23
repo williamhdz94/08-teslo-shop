c37544fb73b82783e446ca0fec7e695959ed21c0
williamhdz94 <williamhernandez450@gmail.com>
2025-07-23 06:37:00 -0600

    path alias

---
+++ src/app/store-front/components/product-card/product-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'product-card',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './product-card.component.html',
+})
+export class ProductCardComponent { }
